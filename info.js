26fda1e1c2ce43617a1a3d8c26418ad14a6d6a2a
Aacgectyuoki <maximiliandellthib@mail.adelphi.edu>
2021-03-17 14:05:39 -0400

    Add files via upload

--- info.js
+++ info.js
@@ -1,83 +1,57 @@
-var empData=[];
-function storeInSession() {
-   // localStorage.setItem("empInfo",JSON.stringify(empData));
-   sessionStorage.setItem("empInfo",JSON.stringify(empData));
-}
-function retrieveFromSession() {
-    //var obj = JSON.parse(localStorage.getItem("empInfo"));
-    var obj = JSON.parse(sessionStorage.getItem("empInfo"));
-    console.log(obj);
-}
-function onFormSubmit() {
-    var formData = addBlog();
-    //insertNewRecords(formData);
-    resetForm();
-    empData.push(formData);
-    console.log(empData)
+var blog=[];
+
+function submitBlog() {
+    insertNewBlog();
+    displayBlog();
 }
 
-function addBlog(){
-  /**var formData={};
-  formData.title=document.getElementById("titleInfo").value;
-  formData.desc=document.getElementById("descInfo").value;
-  return formData;**/
-    var formData={};
-    formData.title = document.getElementById("title").value;
-    formData.desc = document.getElementById("desc").value;
-    var imageInfo = document.getElementById("imageId").files[0].name;
-    console.log(formData.title)
-    console.log(formData.desc);
-    console.log(imageInfo);
-    document.getElementById("titleInfo").innerHTML=formData.title;
-    document.getElementById("descInfo").innerHTML=formData.desc;
-    document.getElementById("imageInfo").src=imageInfo;
-    return formData;
+function readFormData() {
+  var formData={};
+  formData.title = document.getElementById("title").value;
+  formData.desc = document.getElementById("desc").value;
+  formData.imageId = document.getElementById("imageId").files[0].name;
+  console.log(formData);
+  return(formData);
 }
 
-/**
-function readData() {
-    var formData={};
-    formData.title=document.getElementById("titleInfo").innerHTML;
-    formData.desc=document.getElementById("descInfo").innerHTML;
-    return formData;
-}**/
+// use array to store data
+// one function to retrieve latest values from local localStorage
+// one function to insert value from local localStorage
+// one function to display your blog listing, you have to use loop inside it
 
-function insertNewRecords(formData) {
-    var table = document.getElementById("employeeDetails");
-    var tableBody = table.getElementsByTagName("tbody")[0];
-    var newRow1 = tableBody.insertRow(tableBody.length);
-    var newRow2 = tableBody.insertRow(tableBody.length);
-    var newRow3 = tableBody.insertRow(tableBody.length);
-    var cell1 = newRow1.insertCell(0);
-    cell1.innerHTML=formData.title;
-    var cell2 =newRow2.insertCell(0);
-    cell2.innerHTML= formData.desc;
-    var cell3 = newRow3.insertCell(0);
-    cell3.innerHTML="<a href='#' onclick='deleteRec(this)'>Delete Post</a>";
+function displayBlog() {
+  retrieveFromSession();
+	var container = document.getElementById('cardContainer');
+	var content = '';
+  for (let i = 0; i < blog.length; i++) {
+        content += `<div class="card">
+        <img src="${blog[i].imageId}" class="card-img-top">
+        <div class="card-body">
+        <h3 class="card-title">${blog[i].title}</h3>
+        <p class="card-text">${blog[i].desc}</p>
+        </div>
+        </div>
+        `;
+	}
+	container.innerHTML = content;
 }
-/**
-function insertNewRecords(formData) {
-    var table = document.getElementById("employeeDetails");
-    var tableBody = table.getElementsByTagName("tbody")[0];
-    var newColumn = tableBody.insertRow(tableBody.length);
-    var cell1 = newRow.insertCell(0);
-    cell1.innerHTML=formData.name;
-    var cell2 =newRow.insertCell(1);
-    cell2.innerHTML= formData.age;
-    var cell3 = newRow.insertCell(2);
-    cell3.innerHTML="<a href='#' onclick='deleteRec(this)'>X</a>";
-}**/
+
+function retrieveData() {
+  var str = localStorage.getItem("blog");
+  console.log(str);
+  if (str !== null) {
+    blog = JSON.parse(str);
+  }
+}
+
+function insertNewBlog(formData) {
+  retrieveData();
+  var newEntry = readFormData();
+  blog.push(newEntry);
+  localStorage.setItem("blog", JSON.stringify(blog));
+}
 
 function deleteRec(data){
     var row = data.parentElement.parentElement;
     document.getElementById("employeeDetails").deleteRow(row.rowIndex);
-}
-
-function myFunction() {
-  var table = document.getElementById("myTable");
-  var row = table.insertRow(0);
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  cell1.innerHTML = "NEW CELL1";
-  cell2.innerHTML = "NEW CELL2";
 }
